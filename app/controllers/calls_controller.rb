c50e44e90b4b8412db2bd10aa3304641ea6920d8
zayneio <hello@zayne.io>
2018-10-31 22:27:17 -0400

    change start method name in before action exception

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,15 +1,15 @@
 class CallsController < ApplicationController
-  # Note: Twilio recommends disableling the CSRF token check for their webhook:
+  # Note: Twilio recommends disabling the CSRF token check for their webhook:
   # https://www.twilio.com/blog/2016/05/calling-rails-5-getting-started-with-twilio-on-rails.html#h.fgfyo1vvnaqk
   skip_before_action :verify_authenticity_token, except: [:index]
-  before_action :find_call, except: [:index, :call_handler]
+  before_action :find_call, except: [:index, :start]
 
   def index
     @calls = Call.order('created_at DESC')
   end
 
   # Twilio webhook starting point for incoming calls
-  # POST /ivr/welcome
+  # POST /ivr/start
   def start
     @call = Call.create(sid: params[:CallSid], from: params[:Caller], status: params[:CallStatus])
     response = Twilio::TwiML::VoiceResponse.new
@@ -27,9 +27,9 @@ class CallsController < ApplicationController
     case user_selection
     when '1'
       response.say(message: 'Thank you. You selected 1. We will connect you to Zayne\'s direct line now. One moment please.', voice: 'Alice')
-	    response.dial(number: ENV['PHONE_NUMBER'], action: '/ivr/forward', method: 'POST')
+      response.dial(number: ENV['PHONE_NUMBER'], action: '/ivr/forward', method: 'POST')
     when '2'
-	    response.say(message: 'Thank you. You selected 2. Please leave a message after the beep.', voice: 'Alice')
+      response.say(message: 'Thank you. You selected 2. Please leave a message after the beep.', voice: 'Alice')
       response.record(timeout: 10, transcribe: true, action: '/ivr/record', method: 'POST', recordingStatusCallback: '/ivr/after-record')
     else
       response.say(message: 'Unfortunately, it appears you have selected an invalid option. Please call back and try again.', voice: 'Alice')
@@ -37,19 +37,19 @@ class CallsController < ApplicationController
     render xml: response.to_s
   end
 
-  # Redirect for forwarded call selection
+  # Forward call selected
   # Post '/ivr/forward'
   def forward
     @call.update(action: 'forward', status: params[:CallStatus])
   end
 
-  # Redirect for recording messages selections  
+  # Record message selected
   # Post '/ivr/record'
   def record
     @call.update(action: 'record', status: params[:CallStatus])
   end
 
-  # Redirect after recorded message completed
+  # Record message completed
   # Post '/ivr/after-record'
   def after_record
     @call.update(status: 'completed', recording_url: params[:RecordingUrl], duration: params[:RecordingDuration])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'calls#index'
-  post 'ivr/welcome', to: 'calls#start'
+  post 'ivr/start', to: 'calls#start'
   post 'ivr/response', to: 'calls#user_response'
   post 'ivr/forward', to: 'calls#forward'
   post 'ivr/record', to: 'calls#record'
