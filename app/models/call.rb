4016e877ff07a586f46f0855378911b48591a8c3
Julien Malherbe <julien@malherbe.net>
2019-01-26 17:42:06 +0100

    add column for call logging

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: calls
+#
+#  id           :bigint(8)        not null, primary key
+#  country      :string
+#  end_at       :datetime
+#  phone_number :string
+#  provider_sid :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Call < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719183956) do
+ActiveRecord::Schema.define(version: 20190126161604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20170719183956) do
   create_table "calls", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.string "provider_sid"
+    t.string "phone_number"
+    t.datetime "end_at"
   end
 
 end
