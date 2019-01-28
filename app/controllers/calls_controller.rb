3cc366e223ed999264cf1e275906655e87979fc7
Julien Malherbe <julien@malherbe.net>
2019-01-29 00:12:13 +0100

    remove useless before_action of calls controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,7 +1,5 @@
 class CallsController < ApplicationController
 
-  skip_before_action :verify_authenticity_token, only: [:status]
-
   def index
     @calls = Call.includes(:recording).order(created_at: :desc).all
   end
