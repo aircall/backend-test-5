a74d4f31c6ff34e02f708f5056bf60c1ae084ba6
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-04-14 18:39:36 +0200

    required routes are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "call/index"
+    root "calls#index"
+    
+    match 'hello' => 'calls#hello', via: [:get, :post], as: 'hello'
+    match 'aircall' => 'calls#aircall', via: [:get, :post], as: 'aircall'
+    match 'voicemail' => 'calls#voicemail', via: [:get, :post], as: 'voicemail'
+    match 'redirected_call' => 'calls#redirected_call', via: [:get, :post], as: 'redirected_call'    
+    match 'goodbye' => 'calls#goodbye', via: [:get, :post], as: 'goodbye'
+    match 'show/:CallSid' => 'calls#show', via: [:get], as: 'show'
 end
