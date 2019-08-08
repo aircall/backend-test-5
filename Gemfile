1b3d6baa2db10416aceed7f5a38d6bb29f8fc8fc
Fabienne <fabienne@fidzup.com>
2019-08-08 10:11:48 +0200

    add kaminari gem for pagination, modify callscontroller adding page and add calls index view

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'twilio-ruby', '~> 5.25.3'
 # Figaro gem 
 gem 'figaro'
 
+# Pagination
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,6 +3,7 @@
 # Controller for Calls : create method receive new phone call
 class CallsController < ApplicationController
   def index
-    @call = Call.all.order('created_at DESC').page(page_params[:page]).per(25)
+    @calls = Call.all.order('created_at DESC').page params[:page]
   end
+
 end
