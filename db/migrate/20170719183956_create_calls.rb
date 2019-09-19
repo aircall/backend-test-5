efd545a4a3b2a5fe684c1135fa7f69b7cc6cf843
tomh <tom.holovacko@gmail.com>
2019-09-19 13:02:06 -0400

    add columns for activity feed

--- db/migrate/20170719183956_create_calls.rb
+++ db/migrate/20170719183956_create_calls.rb
@@ -1,7 +1,10 @@
 class CreateCalls < ActiveRecord::Migration[5.1]
   def change
     create_table :calls do |t|
-
+      t.string :status
+      t.integer :duration
+      t.string :voicemail_url
+      t.string :caller
       t.timestamps
     end
   end
